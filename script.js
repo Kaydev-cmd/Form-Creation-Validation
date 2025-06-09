a213028abd2c2fbf7fb8e44a8a0837865571d77d
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-09 12:52:28 +0200

    feat: wrap entire scrip inside DOMContentLoaded and add submit event handler

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-const form = document.getElementById("registration-form");
-const feedbackDiv = document.getElementById("form-feedback");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
 
-form.addEventListener((e) => {
-  e.preventDefault();
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  const username = document.getElementById("username").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("password").value.trim();
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
 
-  let isValid = true;
-  const messages = [];
+    let isValid = true;
+    const messages = [];
 
-  if (username.length < 3) {
-    isValid = false;
-    messages.push("Please enter a username that's 3 or more characters long");
-  }
+    if (username.length < 3) {
+      isValid = false;
+      messages.push("Please enter a username that's 3 or more characters long");
+    }
 
-  if (!email.includes("@") || !email.includes(".")) {
-    isValid = false;
-    messages.push("Please include '@' and '.' in your email");
-  }
+    if (!email.includes("@") || !email.includes(".")) {
+      isValid = false;
+      messages.push("Please include '@' and '.' in your email");
+    }
 
-  if (password.length < 8) {
-    isValid = false;
-    messages.push("Password should be 8 or more characters long");
-  }
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("Password should be 8 or more characters long");
+    }
 
-  feedbackDiv.style.display = "block";
+    feedbackDiv.style.display = "block";
 
-  if (isValid) {
-    feedbackDiv.textContent = "Registration successful!";
-    feedbackDiv.style.color = "#28a745";
-  }
+    if (isValid) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745";
+    }
+  });
 });
