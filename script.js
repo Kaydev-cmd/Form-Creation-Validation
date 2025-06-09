59489a1791e06ed703bf7a369ea334c2edc50f0a
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-09 12:25:08 +0200

    feat: retrieve and trim input values for username, email, and password

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const feedbackDiv = document.getElementById("form-feedback");
 
 form.addEventListener((e) => {
   e.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
 });
