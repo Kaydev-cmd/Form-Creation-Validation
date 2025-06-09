18434ece8650eb93df3dd915c082016a7523aa08
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-09 12:30:09 +0200

    feat: initialize validation state and message storage for inputs

--- script.js
+++ script.js
@@ -7,4 +7,7 @@ form.addEventListener((e) => {
   const username = document.getElementById("username").value.trim();
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
+
+  let isValid = true;
+  const messages = [];
 });
