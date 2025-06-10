40aad7b4668053d0a0eaf899d295b62e52dd1bc7
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-10 09:38:33 +0200

    feat: render fetched user names as a list in the DOM

--- fetch-data.js
+++ fetch-data.js
@@ -5,7 +5,18 @@ async function fetchUserData() {
   try {
     const response = await fetch(apiUrl);
     const users = await response.json();
+    dataContainer.innerHTML = "";
+    const userList = document.createElement("ul");
+
+    users.forEach((user) => {
+      const liElement = document.createElement("li");
+      liElement.textContent = user.name;
+      userList.append(liElement);
+    });
+    dataContainer.append(userList);
   } catch (error) {
     console.error(error);
   }
 }
+
+document.addEventListener("DOMContentLoaded", fetchUserData);
